bbb2b9df5a52c34c967ba0bb7b15b59018fd1345
csabi <nemeth.csaba@revolve.hu>
2019-02-27 17:13:37 +0100

    sss

---
+++ plot_proba.py
@@ -0,0 +1,50 @@
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+# os.chdir('c:/')
+
+# print (os.path.isabs(sys.executable))
+# print (sys.argv)
+
+# sys.path.append('e:\Temp')
+for i in sys.path:
+    pass
+    # print (i)
+
+import proba
+sor = ['1', '2']
+cc = proba.valami(sor)
+
+
+
+print (sor)
+
+
+exit()
+sss = proba.Valamiclass()
+print (sss.ladd)
+# print (proba.Valamiclass.ladd)
+
+
+exit()
+
+
+
+
+
+
+
+
+
+plt.ioff()
+plt.plot([0, 1.6, 2.7])
+plt.show()
+
+exit()
+plt.ioff()
+for i in range(1):
+    plt.plot(np.random.rand(6))
+    plt.show()
